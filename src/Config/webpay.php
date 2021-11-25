68838bd58b348eb6d3ac791e652ae0ae5dbc6c12
Jorge Fuentes <j.fuentes.delaguarda@gmail.com>
2021-11-25 15:58:54 -0300

    update config file

--- src/Config/webpay.php
+++ src/Config/webpay.php
@@ -1,8 +1,8 @@
 <?php
 
 return [
-    'WEBPAY_RETURN_URL'     => env('WEBPAY_RETURN_URL', 'http://127.0.0.1:8000/webpay'),
-    'WEBPAY_TESTING'        => env('WEBPAY_TESTING', true),
-    'WEBPAY_API_KEY'        => env('WEBPAY_APP_KEY_PATH', null),
-    'WEBPAY_COMMERCE_CODE'  => env('WEBPAY_COMMERCE_CODE', null)
+    'RETURN_URL'     => env('WEBPAY_RETURN_URL', 'http://127.0.0.1:8000/webpay'),
+    'TESTING'        => env('WEBPAY_TESTING', true),
+    'API_KEY'        => env('WEBPAY_APP_KEY_PATH', null),
+    'COMMERCE_CODE'  => env('WEBPAY_COMMERCE_CODE', null)
 ];
