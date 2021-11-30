05ad2342d7b12e2cda24053c95cec2e987c61b6a
Jorge Fuentes <j.fuentes.delaguarda@gmail.com>
2021-11-30 17:43:06 -0300

    ??

---
+++ src/WebpayPlus.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Crealab\WebpayPlusPaymentGateway;
+
+use Crealab\WebpayPlusPaymentGateway\WebpayPlusGateway;
+
+class WebpayPlus{
+    public function __call($method, $parameters)
+    {
+        return (new WebpayPlusGateway)->$method(...$parameters);
+    }
+
+
+    public static function __callStatic($method, $parameters)
+    {
+        $instance = new static;
+        return call_user_func( [$instance, $method], $parameters );
+    }
+}
