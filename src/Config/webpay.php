6a42aa87dd32a9314252170b630c42d4006f80ee
Jorge Fuentes <j.fuentes.delaguarda@gmail.com>
2022-05-15 20:43:52 -0400

    Update webpay.php

--- src/Config/webpay.php
+++ src/Config/webpay.php
@@ -9,7 +9,7 @@ return [
     ],
     'mall' => [
         'return_url'     => env('WEBPAY_MALL_RETURN_URL', '/webpay-mall'),
-        'debug'          => env('WEBPAY_PLUS_DEBUG', true),
-        'commerce_code'  => env('WEBPAY_PLUS_COMMERCE_CODE', null)
+        'debug'          => env('WEBPAY_MALL_DEBUG', true),
+        'commerce_code'  => env('WEBPAY_MALL_COMMERCE_CODE', null)
     ]
 ];
